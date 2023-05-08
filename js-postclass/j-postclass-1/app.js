61f24a7f529824f64f03ba027d5438b94e7c2b6f
blue06900 <bulenterdgan8186@gmail.com>
2023-05-08 12:12:01 +0300

    ss

---
+++ js-postclass/j-postclass-1/app.js
@@ -0,0 +1,13 @@
+var deneme = 5;
+var Deneme = 6;
+const myVar1 = 7;
+
+
+
+document.getElementById("aritmetik").innerHTML = "deneme = " + deneme;
+
+document.getElementById("aritmetik").innerHTML += "<br>"+ "Deneme = " + Deneme;
+
+const adi = "Cafer";
+const soyadi = "Cambaz";
+document.getElementById("aritmetik").innerHTML += "<br>"+ adi + " " + soyadi
